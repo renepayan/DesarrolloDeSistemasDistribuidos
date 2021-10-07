7814a16ed3d14489f35b15f174d2c1029e085460
René Payán Téllez <payantellez1g@hotmail.com>
2021-10-07 18:01:43 -0500

    Agregada la clase del 7 de octubre

---
+++ Clases/2021/10/07/ServidorRMI.java
@@ -0,0 +1,8 @@
+import java.rmi.*;
+public class ServidorRMI{
+    public static void main(String[] args) throws Exception{
+        String url = "rmi://localhost/prueba";
+        ClaseRMI obj = new ClaseRMI();
+        Naming.rebind(url, obj);
+    }
+}
